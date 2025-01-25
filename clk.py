c69bd16283616b11e3349b0a301a5dc17ced5790
Kai <kai.lifemail@gmail.com>
2025-01-25 22:45:55 +0800

    Create clk.py

---
+++ clk.py
@@ -0,0 +1,18 @@
+//一个时钟函数
+
+import time
+
+minutes = int(input("Enter time"))
+total_seconds = minutes * 60
+
+while total_seconds:
+    mins, secs = divmod(total_seconds, 60)
+    timer = '{:02d}:{:02d}'.format(mins, secs)
+    print(timer, end="\r")
+    time.sleep(1)
+    total_seconds -= 1
+print("Focus timer complete!")
+
+
+//2024.1.23
+//18.57
